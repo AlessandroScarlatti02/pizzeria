f396a4d65c5ff94cea80b5b88e91213d77b4d30d
AlessandroScarlatti02 <alessandro.scarlatti2002@gmail.com>
2024-11-06 00:27:52 +0100

    Fist Commit

---
+++ src/main/java/it/lessons/pizzeria/controller/PizzeController.java
@@ -0,0 +1,24 @@
+package it.lessons.pizzeria.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import it.lessons.pizzeria.repository.PizzaRepository;
+
+@Controller
+@RequestMapping("/pizze")
+public class PizzeController {
+
+    @Autowired
+    private PizzaRepository pizzaRepo;
+
+    @GetMapping
+    public String index(Model model) {
+
+        model.addAttribute("pizze", pizzaRepo.findAll());
+        return "pizze/index";
+    }
+
+}
---
+++ src/main/java/it/lessons/pizzeria/model/Pizza.java
@@ -0,0 +1,71 @@
+package it.lessons.pizzeria.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pizze")
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private String description;
+
+    private String pic;
+
+    private double price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza [id=" + id + ", name=" + name + ", description=" + description + ", pic=" + pic + ", price="
+                + price + "]";
+    }
+}
